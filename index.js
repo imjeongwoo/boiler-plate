1681897b34a2a9386b24419bbe35c34e03390198
imjeongwoo <limjeong1219@gmail.com>
2021-07-20 15:17:02 +0900

    Update .gitignore

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://imjeongwoo:qwer1234@cluster0.or8oa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('ddddd')
 });
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+    })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
